b8403e7965985a5b8126846ceb5bb9778e95df00
thamirissilva <thamirisilva97@gmail.com>
2018-04-02 21:32:42 -0300

    aplicando css nas views

--- view/viewclientes.php
+++ view/viewclientes.php
@@ -1,9 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Clientes</title>
+
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link type="text/css" rel="stylesheet" href="../css/materialize.css"/>
+	<meta name="viewport" content="width=device-width, inicial-scale=1.0">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </head>
+
 <body>
+
+	<style type="text/css">
+			main{
+				background-color: white;
+				/*opacity: 0.9;*/
+				height: 100%;
+				margin-left:1%;
+				margin-right:1%;
+				padding-left: 1%;
+				padding-right: 1%;
+				border-radius: 10px;			
+			}
+	</style>
+ 
+ 
+
+	<div>	
+
 	<!-- Inserindo arquivos -->
 	<?php 
 		include("../conexao.php"); 
@@ -13,9 +38,29 @@
 		$sql = "SELECT *FROM tbcliente";
 		$consulta = mysqli_query($conn, $sql);
 	?>
+</div>
 
-	<!-- Botão Novo Cliente -->
-		<a href="viewnovocliente.php"> Novo Cliente </a>
+<main>
+	
+<div class="row">
+		
+	<br>
+	<div>
+	 <!-- Botão Novo Cliente -->
+		<a href="viewnovocliente.php" class="botao"> Novo Cliente </a>
+	</div>
+
+	<br>
+	<br>
+
+
+	<div class="tabela">
+	<style type="text/css">
+		div .tabela td{
+			font-family: verdana;
+			color: #900;
+		}
+	</style>
 
 	<!-- Tabela Clientes -->
 	<?php
@@ -42,5 +87,38 @@
 			
 		echo "</table>";
 ?>
+
+</div>
+
+	</div>
+
+</main>
+<br>
+
+<footer class="page-footer red darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Sobre Nós</h5>
+                <p class="grey-text text-lighten-4">TEXTO</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          	
+            <div class="container">
+            © 2018 Copyright
+            </div>
+         <br>
+        </footer>
+            
 </body>
 </html>
--- view/viewnovocliente.php
+++ view/viewnovocliente.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Novo Cliente</title>
+
+	<!-- Fontes do google -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../css/materialize.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<meta name="viewport" content="width=device-width, inicial-scale=1.0">
+
 </head>
 <body>
-	<h1> Novo Cliente </h1>
+	
+<nav class="red darken-4">
+	<ul>
+		<li><a href="../index.php"> Home </a></li>
+		<li><a href="viewprodutos.php"> Produtos  </a></li>
+		<li><a href="viewclientes.php"> Clientes </a></li>
+		<li><a href="../model/pedidos.php"> Pedidos </a></li>
+		<li><a href="../model/carrinho.php"> Carrinho</a></li>
+	</ul>
+</nav>
 
-	<form action="../controller/createcliente.php" method="POST">
+
+<main>
+
+<div>
+	<!-- Formulário de clientes -->
+
+	<form action="../controller/createcliente.php" method="POST" class="form">
+
+		<h1> Novo Cliente </h1>
+
 		Nome:
 			<input type="text" name="nome"/>
 		<br/>
@@ -17,7 +42,36 @@
 			<input type="text" name="fone">
 		<br/>
 
-		<input type="submit" name="Cadastrar">
+		<button class="botao">Cadastrar</button>
 	</form>
+</div>
+
+</main>
+
+<footer class="page-footer red darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Sobre Nós</h5>
+                <p class="grey-text text-lighten-4">TEXTO</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          
+            <div class="container">
+            © 2018 Copyright
+            </div>
+         <br>
+        </footer>
+
 </body>
 </html>
